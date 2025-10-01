1006b7745e918d00628650f2e3b0040fb0b18e63
Asher Wayde <ashbwayde@gmail.com>
2025-10-01 13:22:41 -0400

    editing user functions

---
+++ screens/UserFuncts/Register.js
@@ -0,0 +1,13 @@
+import {Text,View,} from 'react-native';
+
+function Register() 
+{
+    return 
+    (
+        <View>
+            <Text>Register</Text>
+        </View>
+    );
+}
+
+export default Register;
